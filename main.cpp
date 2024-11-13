014c9b7881c9b0253238b5f01a51d6b12c624a23
Pavel <kat.pasha0@gmail.com>
2024-11-13 22:11:45 +0500

    Реализован "каркас" программы

--- main.cpp
+++ main.cpp
@@ -1,9 +1,71 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
 
 using namespace std;
 
+
+ifstream in;
+ofstream out;
+
+string str;
+vector<string> myVector;
+
+void read()
+{
+
+}
+
+void out1()
+{
+
+}
+
+void write()
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Ok" << endl;
+    read(); out1(); write();
+
     return 0;
 }
+
+/*
+void read()
+{
+    in.open("text.txt");
+    if (in.is_open())
+    {
+        while (getline(in, str))
+        {
+            myVector.push_back(str);
+        }
+    }
+}
+
+void out1()
+{
+    cout << myVector[0] << endl << myVector[1] << endl << myVector[2] << endl;
+}
+
+void write()
+{
+    out.open("out.txt");
+    if (out.is_open())
+    {
+        for (int i = 0; myVector.size(); i++)
+        {
+            out << myVector[i] << endl;
+        }
+    }
+    out.close();
+}
+
+*/
